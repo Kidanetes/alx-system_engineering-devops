af883f6072f7abd136ef85b80269d20487bf49b1
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-08-13 15:04:22 +0300

    advanced api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""advanced api"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
