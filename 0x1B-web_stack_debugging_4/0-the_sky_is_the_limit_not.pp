9e37f1947784f7abde596819ac1f8c06422aa22d
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-08-22 16:02:19 +0300

    restart the nginx server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 #increase the amount of traffic
-
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
+#restart the nginx server
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
